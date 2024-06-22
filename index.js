f6a9df39e2d288e4c71c336cbfb7a9da22b5170c
Corcholog <loguerciogiorgioivan@gmail.com>
2024-06-22 13:33:25 -0300

    Signed-off-by: Corcholog <loguerciogiorgioivan@gmail.com>

---
+++ index.js
@@ -0,0 +1,29 @@
+const http = require('http')
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+
+    if(req.url === "/"){
+        res.write("WElcome")
+        return res.end();
+    }
+
+    if (req.url === "/Home") {
+        const filePath = path.join(__dirname, 'home.html');
+        fs.readFile(filePath, (err, content) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.write("Server Error");
+                return res.end();
+            }
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(content);
+            return res.end();
+        });
+    }
+
+})
+
+server.listen(3000)
+console.log("Ruge, puta!")
